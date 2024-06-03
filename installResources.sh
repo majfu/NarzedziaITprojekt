93b443601eae7b26c519f850e85b503ede63a1ac
majfu <163520854+majfu@users.noreply.github.com>
2024-06-03 16:33:55 +0200

    added .sh to main

---
+++ installResources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -m pip install xmltodict
+python3 -m pip install PyYAML
+python3 -m pip install aiofiles
+python3 -m pip install async_tkinter_loop
+python3 -m pip install pyinstaller
